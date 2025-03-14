4a0a18b1746dda9bc0460a1685c6f8515813fdad
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-03-13 22:59:40 -0300

    criação de classe main para pacote das questoes 9 e 10

---
+++ src/questao9e10/Main.java
@@ -0,0 +1,11 @@
+package questao9e10;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario fun1 = new Funcionario("kanye");
+        Funcionario fun2 = new Funcionario();
+        Funcionario fun3 = new Funcionario();
+        Funcionario fun4 = new Funcionario();
+        Funcionario fun5 = new Funcionario();
+    }
+}
