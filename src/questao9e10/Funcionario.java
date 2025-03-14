bcedaec0d71bdf79612f18fabf9921e7ce70702a
kauê <atsimplantacaovdc@gmail.com>
2025-03-14 09:41:36 -0300

    finalização da questão 10

--- src/questao9e10/Funcionario.java
+++ src/questao9e10/Funcionario.java
@@ -5,10 +5,9 @@ public class Funcionario {
     private String sobrenome;
     private double horasTrabalhadasMes;
     private double valorHoraTrabalhada;
-    private double salarioBase;
 
     // construtor
-    Funcionario(String nome, String sobrenome, double salario, double valorHoraTrabalhada) {
+    Funcionario(String nome, String sobrenome, double valorHoraTrabalhada) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.horasTrabalhadasMes = 0;
@@ -20,22 +19,21 @@ public class Funcionario {
         horasTrabalhadasMes += h;
     }
 
-    double getSalarioLiquido() { // jeito dificil
+    double getSalarioLiquido() {
         double salBase = horasTrabalhadasMes * valorHoraTrabalhada;
         double descontos = 0;
-        if (salBase <= 7087.22 && salBase > 3641.04) {
-            descontos = descontos + (salBase - (salBase * 0.14));
+
+        if (salBase > 7087.22) {
+            descontos = salBase * 0.14;
+        } else if (salBase > 3641.04) {
+            descontos = salBase * 0.12;
+        } else if (salBase > 2427.36) {
+            descontos = salBase * 0.09;
+        } else if (salBase >= 1212.01) {
+            descontos = salBase * 0.075;
         }
-        if (salBase <= 3641.03 && salBase >= 2427.36) {
-            descontos = descontos + (salBase - (salBase * 0.12));
-        }
-        if (salBase <= 2427.35 && salBase >= 1212.01) {
-            descontos = descontos + (salBase - (salBase * 0.09));
-        }
-        if (salBase <= 1212.00) {
-            descontos = descontos + (salBase - (salBase * 0.075));
-        }
-        return salBase - descontos;
+
+        return (salBase - descontos);
     }
 
 
