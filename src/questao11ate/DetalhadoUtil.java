9f3e6b5955250ce59f89391f6e8e075e2b25f5aa
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-03-15 23:00:50 -0300

    dropei o restante da lista a partir daqui, cansado demais pra terminar no momento... e também é uma lista de revisao

---
+++ src/questao11ate/DetalhadoUtil.java
@@ -0,0 +1,20 @@
+package questao11ate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class DetalhadoUtil {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Date dateHora = null;
+
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
+
+    }
+
+
+}
